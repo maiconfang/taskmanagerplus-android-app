a46d4ede4aa1d28411aa4b55bfdf226abe5123b5
Maicon <maicon.fang@gmail.com>
2024-10-07 16:17:24 -0300

    Added new layout and activity files for TaskDashboard implementation.

---
+++ app/TaskManagerPlus/app/src/main/java/maif/taskmanagerplus/ui/dashboard/TaskDashboardActivity.kt
@@ -0,0 +1,22 @@
+package maif.taskmanagerplus.ui.dashboard
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import maif.taskmanagerplus.R
+
+class TaskDashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_task_dashboard)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
