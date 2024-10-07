1bdcfb3ac0e97046a12aac9cca66f13437056234
Maicon <maicon.fang@gmail.com>
2024-10-07 11:13:54 -0300

    Initial commit: Starting the development of the Task Manager Plus Android app. Implemented the login flow with validation and navigation to the main screen after successful login.

---
+++ app/TaskManagerPlus/app/src/main/java/gmta/gethugo/taskmanagerplus/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package gmta.gethugo.taskmanagerplus.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState (val usernameError: Int? = null,
+                      val passwordError: Int? = null,
+                      val isDataValid: Boolean = false)
